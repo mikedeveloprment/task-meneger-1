04cf1d4b92ce525c55d85b9d407e979280525dc4
mikedeveloprment <mikebahgneiy@gmail.com>
2023-08-20 19:35:34 +0300

    fix-8

--- src/pages/Home/Tags/Tags.jsx
+++ src/pages/Home/Tags/Tags.jsx
@@ -4,10 +4,13 @@ import clas from "./Tags.module.scss";
 const Tags = () => {
 	return (
 		<ul className={clas.list}>
-			<li className={clas.item}>travel</li>
-			<li className={clas.item}>natural</li>
-			<li className={clas.item}>1200/900</li>
-			<li className={clas.item}>top</li>
+			<li className={clas.tag}>
+				<p className={clas.text}>travel</p>
+				<button className={clas.button}>
+					<span className={`${clas.line} ${clas.line1}`}></span>
+					<span className={`${clas.line} ${clas.line2}`}></span>
+				</button>
+			</li>
 		</ul>
 	);
 };
