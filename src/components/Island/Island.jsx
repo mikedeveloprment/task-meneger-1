bbc7743a74dfc76ee303472951666e02d3d011be
mikedeveloprment <mikebahgneiy@gmail.com>
2023-08-26 22:50:41 +0300

    3

--- src/components/Island/Island.jsx
+++ src/components/Island/Island.jsx
@@ -4,7 +4,14 @@ import clas from "./Island.module.scss";
 const Island = () => {
 	return (
 		<div className={clas.wrapper}>
-			<nav className={`${clas.nav} black1-bg`}></nav>
+			<nav className={`${clas.nav} black1-bg`}>
+				<div className={clas.cont}>
+					<button className={clas.increment}></button>
+				</div>
+				<p className={clas.text}>Create new task</p>
+				<button className={`${clas.button} gray2-bg`}></button>
+				<button className={`${clas.button} gray2-bg`}></button>
+			</nav>
 		</div>
 	);
 };
